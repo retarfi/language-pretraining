d6722125e07a431039e7b9997a161e7f90f55965
retarfi <valerososoccer@gmail.com>
2021-08-19 22:35:14 +0900

    update: having as many sentences as possible in LineByLineTextDataset

--- utils/language_modeling.py
+++ utils/language_modeling.py
@@ -4,7 +4,6 @@ This file is from transformers.data.datasets.data_collator
 
 from filelock import FileLock
 import glob
-from multiprocessing import Pool
 import os
 import pickle
 import random
@@ -242,13 +241,15 @@ class TextDatasetForNextSentencePrediction(Dataset):
 
 
 # Override transformers.data.datasets.LineByLineTextDataset
-# add multiprocessing
 class LineByLineTextDataset(Dataset):
     """
     This will be superseded by a framework-agnostic approach soon.
     """
 
-    def __init__(self, tokenizer: PreTrainedTokenizer, file_path: str, block_size: int):
+    def __init__(
+        self, tokenizer: PreTrainedTokenizer, file_path: str, block_size: int,
+        overwrite_cache:bool = False, short_seq_probability:float = 0.1,
+    ):
         warnings.warn(
             DEPRECATION_WARNING.format(
                 "https://github.com/huggingface/transformers/blob/master/examples/pytorch/language-modeling/run_mlm.py"
@@ -256,18 +257,140 @@ class LineByLineTextDataset(Dataset):
             FutureWarning,
         )
         assert os.path.isfile(file_path) or os.path.isdir(file_path), f"Input file or directory path {file_path} not found"
-        # Here, we do not cache the features, operating under the assumption
-        # that we will soon use fast multithreaded tokenizers from the
-        # `tokenizers` repo everywhere =)
-        logger.info(f"Creating features from dataset file at {file_path}")
-        if os.path.isfile(file_path):
-            examples = batch_encoding(file_path, tokenizer, block_size)
-            self.examples = [{"input_ids": torch.tensor(e, dtype=torch.long)} for e in examples]
-        else:
-            with Pool(os.cpu_count()) as pool:
-                mp_task = [pool.apply_async(batch_encoding, (file_path_i, tokenizer, block_size)) for file_path_i in glob.glob(os.path.join(file_path, '*.txt'))]
-                examples = [f.get() for f in mp_task]
-            self.examples = [{"input_ids": torch.tensor(e, dtype=torch.long)} for doc in examples for e in doc]
+
+        self.short_seq_probability = short_seq_probability
+
+        directory, filename = os.path.split(file_path)
+        cached_features_file = os.path.join(
+            directory,
+            f"cached_lbl_{tokenizer.__class__.__name__}_{block_size}_{filename}",
+        )
+
+        self.tokenizer = tokenizer
+
+        # Make sure only the first process in distributed training processes the dataset,
+        # and the others will use the cache.
+        lock_path = cached_features_file + ".lock"
+
+        # Input file format:
+        # (1) One sentence per line. These should ideally be actual sentences, not
+        # entire paragraphs or arbitrary spans of text. (Because we use the
+        # sentence boundaries for the "next sentence prediction" task).
+        # (2) Blank lines between documents. Document boundaries are needed so
+        # that the "next sentence prediction" task doesn't span between documents.
+        #
+        # Example:
+        # I am very happy.
+        # Here is the second sentence.
+        #
+        # A new document.
+
+        with FileLock(lock_path):
+            if os.path.exists(cached_features_file) and not overwrite_cache:
+                start = time.time()
+                with open(cached_features_file, "rb") as handle:
+                    self.examples = pickle.load(handle)
+                logger.info(
+                    f"Loading features from cached file {cached_features_file} [took %.3f s]", time.time() - start
+                )
+                print(max([len(x['input_ids']) for x in self.examples]))
+
+            else:
+                logger.info(f"Creating features from dataset file at {directory}")
+
+                self.documents = [[]]
+                with open(file_path, encoding="utf-8") as f:
+                    while True:
+                        line = f.readline()
+                        if not line:
+                            break
+                        line = line.strip()
+
+                        # Empty lines are used as document delimiters
+                        if not line and len(self.documents[-1]) != 0:
+                            self.documents.append([])
+                        tokens = tokenizer.tokenize(line)
+                        tokens = tokenizer.convert_tokens_to_ids(tokens)
+                        if tokens:
+                            self.documents[-1].append(np.array(tokens, dtype='int32'))
+
+                logger.info(f"Creating examples from {len(self.documents)} documents.")
+                self.examples = []
+                for doc_index, document in enumerate(self.documents):
+                    self.create_examples_from_document(document, doc_index, block_size)
+
+                start = time.time()
+                with open(cached_features_file, "wb") as handle:
+                    pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                logger.info(
+                    f"Saving features into cached file {cached_features_file} [took {time.time() - start:.3f} s]"
+                )
+
+    # Overwride TextDatasetForNextSentencePrediction.create_examples_from_document
+    def create_examples_from_document(self, document: List[np.ndarray], doc_index: int, block_size: int):
+        """Creates examples for a single document."""
+
+        max_num_tokens = block_size - self.tokenizer.num_special_tokens_to_add(pair=True)
+
+        # We *usually* want to fill up the entire sequence since we are padding
+        # to `block_size` anyways, so short sequences are generally wasted
+        # computation. However, we *sometimes*
+        # (i.e., short_seq_prob == 0.1 == 10% of the time) want to use shorter
+        # sequences to minimize the mismatch between pretraining and fine-tuning.
+        # The `target_seq_length` is just a rough target however, whereas
+        # `block_size` is a hard limit.
+        target_seq_length = max_num_tokens
+        if random.random() < self.short_seq_probability:
+            target_seq_length = random.randint(2, max_num_tokens)
+
+        current_chunk = []  # a buffer stored current working segments
+        current_length = 0
+        i = 0
+
+        while i < len(document):
+            segment = document[i].tolist()
+            current_chunk.append(segment)
+            current_length += len(segment)
+            if i == len(document) - 1 or current_length >= target_seq_length:
+                if current_chunk:
+                    if len(current_chunk) >= 2 and current_length > target_seq_length:
+                        # We didn't actually use these segments so we "put them back" so
+                        # they don't go to waste.
+                        current_chunk.pop()
+                        i -= 1
+
+                    tokens = []
+                    for ch in current_chunk:
+                        tokens += ch
+
+                    def truncate_seq_pair(tokens, max_num_tokens):
+                        """Truncates a pair of sequences to a maximum sequence length."""
+                        while True:
+                            total_length = len(tokens)
+                            if total_length <= max_num_tokens:
+                                break
+                            assert len(tokens) >= 1
+                            # We want to sometimes truncate from the front and sometimes from the
+                            # back to add more randomness and avoid biases.
+                            if random.random() < 0.5:
+                                del tokens[0]
+                            else:
+                                tokens.pop()
+
+                    truncate_seq_pair(tokens, max_num_tokens)
+
+                    assert len(tokens) >= 1
+
+                    # add special tokens
+                    input_ids = self.tokenizer.build_inputs_with_special_tokens(tokens)
+
+                    # 後でcastする
+                    self.examples.append({"input_ids": torch.tensor(input_ids, dtype=torch.int)})
+
+                current_chunk = []
+                current_length = 0
+
+            i += 1
 
     def __len__(self):
         return len(self.examples)
