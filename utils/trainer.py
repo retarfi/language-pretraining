a878a85a509cd12715d5b792b68e1faa0e50d40b
retarfi <valerososoccer@gmail.com>
2021-08-25 02:05:50 +0900

    update: must save last step

--- utils/trainer.py
+++ utils/trainer.py
@@ -621,6 +621,10 @@ class MyTrainer(Trainer):
             self.control = self.callback_handler.on_epoch_end(args, self.state, self.control)
             self._maybe_log_save_evaluate(tr_loss, model, trial, epoch)
 
+            # THIS IS ADDED BY RETARFI
+            self._save_checkpoint(model, trial, metrics=metrics)
+            self.control = self.callback_handler.on_save(self.args, self.state, self.control)
+
             if DebugOption.TPU_METRICS_DEBUG in self.args.debug:
                 if is_torch_tpu_available():
                     # tpu-comment: Logging debug metrics for PyTorch/XLA (compile, execute times, ops, etc.)
